71567fa9c00fe4cf03beead3c39cee0440cf3efc
cotrez <jules.cotrez@etu.u-paris.fr>
2025-03-15 21:03:39 +0100

    rapport part 2

---
+++ report/structure.ml
@@ -0,0 +1,27 @@
+type plateforme = {
+  platform_x : int;
+  platform_y : int;
+  platform_width : int;
+  platform_height : int;
+}
+
+type entities = {
+  player : joueur;
+  ennemis : ennemi list;
+  plateforme_list : plateforme list;
+}
+
+type joueur = {
+  nom : string;
+  pos : position;
+  vector_velocity : float * float;
+  health_point : int;
+  attack_point : int;
+  jetpack_carburant_pourcentage : int;
+  grap : grappin;
+  sprite : string;
+  height : float;
+  width : float;
+  facing_right : bool;
+  falling : bool
+}
